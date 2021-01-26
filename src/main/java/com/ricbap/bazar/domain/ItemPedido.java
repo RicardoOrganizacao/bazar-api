2b3b2b71fa5e934d95e19cf89ac36e0e7b892956
Ricardo <ricbapdevs@gmail.com>
2021-01-26 08:32:36 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/ricbap/bazar/domain/ItemPedido.java
+++ src/main/java/com/ricbap/bazar/domain/ItemPedido.java
@@ -43,6 +43,11 @@ public class ItemPedido implements Serializable {
 		return id.getProduto();
 	}
 	
+	// Soma de subtotal deserializado pelo Json
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+		
 
 	public ItemPedidoPK getId() {
 		return id;
